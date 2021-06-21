d4702c14226131f93634426c9799fd83389861f5
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-21 17:35:00 +0500

    Added an intro message to the terminal.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+	PrintLine("Greetings, traveler!");
+	PrintLine("To type something, press tab then type and hit enter.");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+	ClearScreen();
 }
