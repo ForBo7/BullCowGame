708e9e80d2718107c0f3595844e6ebb6bfd561eb
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-21 21:46:09 +0500

    Wrapped all existing strings in the TEXT macro.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-	PrintLine("Greetings, traveler!");
-	PrintLine("To type something, press tab then type and hit enter.");
+	PrintLine(TEXT("Greetings, traveler!"));
+	PrintLine(TEXT("To type something, press tab then type and hit enter."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 	ClearScreen();
-	FString HiddenWord = "code";
+	FString HiddenWord = TEXT("code");
 }
