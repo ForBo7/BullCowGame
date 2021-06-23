4e4e282f3f86cc6c99b39c20672b0e203e75fe7a
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-23 15:54:28 +0500

    Refreshed the Xcode project and added logic for checking whether the input guess is correct or not.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,16 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-void UBullCowCartridge::BeginPlay() // When the game starts
+// When game starts.
+void UBullCowCartridge::BeginPlay()
 {
     Super::BeginPlay();
 	PrintLine(TEXT("Greetings, traveler!"));
-	PrintLine(TEXT("To type something, press tab then type and hit enter."));
+	// TODO: Remove magic number below.
+	PrintLine(TEXT("Guess the 4 letter word."));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+// When the player hits enter.
+void UBullCowCartridge::OnInput(const FString& Input)
 {
 	ClearScreen();
+	
+	// TODO: Move outside this function.
 	FString HiddenWord = TEXT("code");
-	PrintLine(Input);
+	
+	if (HiddenWord == Input)
+	{
+		PrintLine(TEXT("You guessed the correct word!"));
+	}
+	else
+	{
+		PrintLine(TEXT("You guessed incorrectly!"));
+	}
 }
