ae5ac586d56be7e21263e2912bcebc76831a3e02
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-02 23:05:59 +0500

    Refactored the code and the game can now end and play again.

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,15 +4,60 @@
 // When game starts.
 void UBullCowCartridge::BeginPlay()
 {
-	SetUpGame();
-	
 	Super::BeginPlay();
+	
+	SetUpGame();
 
 	// Debug lines.
 	PrintLine(TEXT("The hidden word is %s."), *HiddenWord);
 	PrintLine(TEXT("It is %i characters long."), HiddenWord.Len());
-	
+}
 
+// When the player hits enter.
+void UBullCowCartridge::OnInput(const FString& Input)
+{
+	if (bGameOver)
+	{
+		ClearScreen();
+		SetUpGame();
+	}
+	else
+	{
+		// Check player guess.
+		if (Input == HiddenWord)
+		{
+			PrintLine(TEXT("You guessed the correct word!"));
+			EndGame();
+		}
+		else
+		{
+			// Check if right number of characters.
+			if (Input.Len() != HiddenWord.Len())
+			{
+				PrintLine(TEXT("The hidden word is %i characters long."),
+						  HiddenWord.Len());
+			}
+			else
+			{
+				PrintLine(TEXT("You guessed incorrectly!"));
+			}
+			// Check if isogram.
+			// If lives > 0, remove life.
+		}
+		// If lives are zero or less, end game.
+		// Print losing message.
+		// Reveal the word.
+	}
+}
+
+void UBullCowCartridge::SetUpGame()
+{
+	HiddenWord = TEXT("code");
+	Lives = HiddenWord.Len();
+	bGameOver = false;
+	
+	
+	// Print intro.
 	PrintLine(TEXT("Greetings, traveler!"));
 	PrintLine(TEXT("Guess the %i letter word."), HiddenWord.Len());
 	PrintLine(TEXT("You have %i lives."), Lives);
@@ -21,36 +66,8 @@ void UBullCowCartridge::BeginPlay()
 	PrintLine(TEXT("Enter to continue."));
 }
 
-// When the player hits enter.
-void UBullCowCartridge::OnInput(const FString& Input)
+void UBullCowCartridge::EndGame()
 {
-	ClearScreen();
-	if (Input == HiddenWord)
-	{
-		PrintLine(TEXT("You guessed the correct word!"));
-	}
-	else
-	{
-		// Check if right number of characters.
-		if (Input.Len() != HiddenWord.Len())
-		{
-			PrintLine(TEXT("The hidden word is %i characters long."),
-					  HiddenWord.Len());
-		}
-		else
-		{
-			PrintLine(TEXT("You guessed incorrectly!"));
-		}
-		// Check if isogram.
-		// If lives > 0, remove life.
-	}
-	// If lives are zero or less, end game.
-	// Print losing message.
-	// Reveal the word.
-}
-
-void UBullCowCartridge::SetUpGame()
-{
-	HiddenWord = TEXT("code");
-	Lives = HiddenWord.Len();
+	bGameOver = true;
+	PrintLine(TEXT("Press enter to play again."));
 }
